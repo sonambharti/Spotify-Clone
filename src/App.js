845cd9d51baf8eb7e1ad83b82d066638ac8debd0
kartikayt <kartikayt7@gmail.com>
2023-07-14 17:06:30 +0530

    Login Via Spotify Implemented using access token

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React,  { useEffect, useState } from "react";
 import './App.css';
+import './App.css';
+import Login from "./Login";
+import { getTokenFromUrl } from './spotify'
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
+    const [token, setToken] = useState(null);
 
-        <
-        /header> <
-        /div>
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        const _token = hash.access_token;
+        window.location.hash= "";
+
+        if (_token) {
+            setToken(_token);
+        }
+
+
+        console.log("Token : ", token);
+    },  []);
+
+
+    return ( 
+    <div className = "App" >
+        {
+            token ? <h1>Logged in</h1> : <Login />
+        }
+    </div>
     );
 }
 
