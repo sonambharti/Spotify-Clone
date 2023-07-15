29746adb24cc0a88dd8428b46932c7b857fe11e4
sonambharti <subodh9955509176@gmail.com>
2023-07-16 04:55:37 +0530

    Sidebar are updated

--- src/App.js
+++ src/App.js
@@ -35,6 +35,13 @@ function App() {
                     user: user,
                 });
             });
+            
+            spotify.getUserPlaylists().then((playlists) => {
+                dispatch({
+                    type: "SET_PLAYLISTS",
+                    playlists: playlists,
+                });  
+            });
         }
 
         // console.log("Token: ", token);
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,9 +4,12 @@ import SidebarOption from './SidebarOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+import { useDataLayerValue } from './DataLayer';
 
 
 function Sidebar() {
+  const [{ playlists }, dispatch] = useDataLayerValue();
+
   return (
     <div className="sidebar">
       <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="" />
@@ -14,6 +17,16 @@ function Sidebar() {
       <SidebarOption title="Home" Icon={HomeIcon}/>
       <SidebarOption title="Search" Icon={SearchIcon} />
       <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+      <br />
+      <strong className="sidebar_title"> PLAYLISTS</strong>
+      <hr/>
+
+      {playlists?.items?.map(playlist => (
+        <SidebarOption title={playlist.name} />
+      ))}
+      {/* <SidebarOption title="Hindi Bollywood" />
+      <SidebarOption title="Bollywood Party Songs" />
+      <SidebarOption title="English Songs" /> */}
     </div>
   );
 }
