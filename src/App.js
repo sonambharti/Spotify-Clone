97acfafdece12e1b7ea61d354b827b1a727553b8
kartikayt <kartikayt7@gmail.com>
2023-07-18 16:02:35 +0530

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         token: _token,
       });
 
-      spotify_new.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+      spotify_new.getPlaylist("37i9dQZEVXcQ9COmYvdajy").then((response) =>
         dispatch({
           type: "SET_DISCOVER_WEEKLY",
           discover_weekly: response,
